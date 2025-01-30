8ed860b46a9115bd9168741359f83852dc385eb7
babargujjar <babargujjaronline@gmail.com>
2025-01-30 14:46:14 +0500

    complete admin panel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/admin/add-completed-projects",
+        destination: "/login",
+        permanent: false,
+      },
+    ];
+  },
+};
