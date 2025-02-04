1ac035ac33b76b136b0d12bc7cd7b50869aa5660
babargujjar <babargujjaronline@gmail.com>
2025-02-04 13:58:18 +0500

    add meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 import { usePathname } from "next/navigation";
 import TopBar from "@/components/TopBar";
 import { metadata } from "./layout.server";
-
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +29,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+        </Head>
         {!isAdminRoute && (
           <>
             <TopBar />
